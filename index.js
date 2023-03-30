03ecaf2cd29938e5a1326d7a269471737d9c3ab5
kpripper <102256496+kpripper@users.noreply.github.com>
2023-03-30 19:35:02 +0300

    2

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-// Write Javascript code!
-const appDiv = document.getElementById('app')
-appDiv.innerHTML = `<h1>JS Starter</h1>`
+const nodemailer = require('nodemailer')
+
+var transporter = nodemailer.createTransport({
+  host: 'smtp.beget.com',
+  port: 25,
+  auth: {
+    user: '000@ladykatrin.ru',
+    pass: 'HOiF9%Wn',
+  },
+})
+
+const mailOptions = {
+  from: '000@ladykatrin.ru',
+  to: '1580509@gmail.com',
+  subject: 'Send using AWS Node.js',
+  text: '...опа!',
+}
+
+try {
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.log(error)
+    } else {
+      console.log('Email sent: ' + info.response)
+    }
+  })
+} catch (error) {
+  console.log(error)
+}
