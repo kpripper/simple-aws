ccda920390c833029af269eaf60621302e264147
kpripper <102256496+kpripper@users.noreply.github.com>
2023-03-30 20:09:13 +0300

    4

--- app.js
+++ app.js
@@ -16,5 +16,5 @@ app.get('/test', function (req, res) {
 })
 
 app.listen(port, function () {
-  console.log('Server running at http://127.0.0.1:%s', port)
+  console.log('Ця хрень запустилася на порту ', port)
 })
